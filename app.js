0d9e87668956149279c207cc51f9b6647ea33d58
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-01 22:26:09 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,19 +2,9 @@ var express = require('express')
 var app = express()
 
 // Load Controllers
-
+const SiteController = require("./controller/SiteController")
 
 // Routes
-
+app.get("/", SiteController.Index)
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
